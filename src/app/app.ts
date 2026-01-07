1388e2803122037f1b7bbc461eb7e940025c6b3d
brian <ugaldebraian3@gmail.com>
2026-01-07 18:40:08 -0300

    creacion y ubicacion de componentes + carga de imagenes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {Header} from './components/header/header';
+import {Footer} from './components/footer/footer';
+import {Carrusel} from './components/carrusel/carrusel';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer ,Carrusel],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
